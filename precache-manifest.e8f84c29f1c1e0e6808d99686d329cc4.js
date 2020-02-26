2f95d5ad96f2226a955696ea5cf9225c982f4dbe
Shahad Alqahtani <shahadalqahtani1993@gmail.com>
2020-02-26 16:11:14 +0300

    Updates

---
+++ precache-manifest.e8f84c29f1c1e0e6808d99686d329cc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc665b73724e2179130422ad4c7a08de",
+    "url": "/Project2/index.html"
+  },
+  {
+    "revision": "80fdbeb482e06ec31f10",
+    "url": "/Project2/static/css/main.44275677.chunk.css"
+  },
+  {
+    "revision": "207e47c84d4bbb068970",
+    "url": "/Project2/static/js/2.b562ffb7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Project2/static/js/2.b562ffb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80fdbeb482e06ec31f10",
+    "url": "/Project2/static/js/main.865900aa.chunk.js"
+  },
+  {
+    "revision": "9bc3dec0b96ce08db6b7",
+    "url": "/Project2/static/js/runtime-main.0da82e9a.js"
+  },
+  {
+    "revision": "206404e18a67ab4a016d3ddce6b52b0f",
+    "url": "/Project2/static/media/feels.206404e1.png"
+  }
+]);
